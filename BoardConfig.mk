7c3a880da1b6234c66dd89f68cd29107b780699b
FUKAUMI Naoki <naobsd@gmail.com>
2012-03-06 13:52:45 +0900

    enable USB mass storage mode in CWM

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,6 +48,7 @@ TARGET_RECOVERY_INITRC := device/allwinner/a10/recovery_init.rc
 #TARGET_RECOVERY_PRE_COMMAND := "echo -n boot-recovery | busybox dd of=/dev/block/nandf count=1 conv=sync; sync"
 #TARGET_RECOVERY_PIXEL_FORMAT := ""
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/allwinner/a10/recovery_keys.c
+BOARD_UMS_LUNFILE := "/sys/class/android_usb/android0/f_mass_storage/lun/file"
 
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
